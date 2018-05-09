3f406b5498f5c862f6a6a2ed3097dbab5ae0b1da
brandl2 <30390704+brandl2@users.noreply.github.com>
2018-05-09 14:49:38 -0400

    Add files via upload

---
+++ Servo.ino
@@ -0,0 +1,38 @@
+/* Example Servo Code
+ by Logan Brand
+ Last Update 05.09.18
+*/
+
+/*
+General Info
+ */
+
+//UNCOMMENT '*' SECTION TO SWEEP SERVO 180 DEGREES ON A LOOP
+
+ 
+//************************************************************************************
+//#include <Servo.h>
+//
+//Servo myservo;    //  creates servo object to control a servo
+//int pos = 0;      //  variable used to store the servo's position 
+//
+//void setup() 
+//{
+//  myservo.attach(9)       //  Using pin 9 as the output
+//}
+//
+//void loop() 
+//{ 
+//  for (pos = 0; pos <= 180; pos += 1) // 0 to 180 degrees, 1 degree steps
+//  { 
+//    myservo.write(pos);               // tell servo to go to position in variable 'pos'
+//    delay(15);                        // waits 15ms for the servo to reach the position
+//  }
+//  
+//  for (pos = 180; pos >= 0; pos -= 1) // 180 to 0 degrees, -1 degree steps
+//  { 
+//    myservo.write(pos);               // tell servo to go to position in variable 'pos'
+//    delay(15);                        // waits 15ms for the servo to reach the position
+//  }
+//}
+//************************************************************************************
